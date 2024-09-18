d5396e963c6612c5a991fda8a74e58b15d0633a8
kiarash kiani <kiarash@datachef.co>
2024-09-18 17:04:45 +0200

    chore(llm): add docstrings

--- src/damavand/cloud/aws/resources/vllm_component.py
+++ src/damavand/cloud/aws/resources/vllm_component.py
@@ -11,14 +11,67 @@ from pulumi import ResourceOptions
 
 @dataclass
 class AwsVllmComponentArgs:
+    """
+    Arguments for the AwsVllmComponent component.
+
+    ...
+
+    Attributes
+    ----------
+    region : str
+        region where the SageMaker model is going to be deployed.
+    model_image_version : str
+        version of the djl-lmi image.
+    model_name : str
+        name of the SageMaker model.
+    instance_initial_count : int
+        number of instances to deploy the model.
+    instance_type : str
+        type of instance to deploy the model.
+    """
+
     region: str = "us-west-2"
     model_image_version: str = "0.29.0"
-    model_name = "microsoft/Phi-3-mini-4k-instruct"
-    instance_initial_count = 1
-    instance_type = "ml.g4dn.xlarge"
+    model_name: str = "microsoft/Phi-3-mini-4k-instruct"
+    instance_initial_count: int = 1
+    instance_type: str = "ml.g4dn.xlarge"
 
 
 class AwsVllmComponent(PulumiComponentResource):
+    """
+    The AwsVllmComponent class is a Pulumi component that deploys an LLM model using vLLM.
+
+    ...
+
+    Attributes
+    ----------
+    name : str
+        the name of the component.
+    args : AwsVllmComponentArgs
+        the arguments of the component.
+    opts : Optional[ResourceOptions]
+        the resource options.
+
+    Methods
+    -------
+    assume_policy()
+        Return the assume role policy for SageMaker.
+    managed_policy_arns()
+        Return a list of managed policy ARNs that defines the permissions for Sagemaker.
+    role()
+        Return an execution role for SageMaker.
+    model_image_ecr_path()
+        Return the ECR image path for the djl-lmi container image serving vllm.
+    model_image_configs()
+        Return the environment configurations for the vllm image.
+    model()
+        Return a SageMaker model.
+    endpoint_config()
+        Return a SageMaker endpoint configuration for the vllm model.
+    endpoint()
+        Return a SageMaker endpoint for the vllm model.
+    """
+
     def __init__(
         self,
         name: str,
